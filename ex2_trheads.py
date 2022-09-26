4c3bc464f5c2383175866d54cded9507db9bcde4
Bruna Oliveira <69090175+bruliveira@users.noreply.github.com>
2022-09-26 09:30:45 -0300

    Update ex2_trheads.py

--- ex2_trheads.py
+++ ex2_trheads.py
@@ -1,5 +1,6 @@
 import threading
 from time import sleep
+import socket
 
 def tarefa(tempo):
     sleep(tempo)
@@ -11,3 +12,25 @@ print("Continuando....")
 sleep(10)
 print("Continuando main thread...")
 t1.join()
+
+
+
+def atender_cleinte(client):
+    data = client.recv(2048)
+    client.semd(data)
+    client.close()
+
+
+def server(host='localhost', port=8082):
+    sock=socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SUL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = (host, port)
+    sock.binD(server_address)
+    sock.listen(5)
+
+    while True:
+        client, address = sock.accept()
+        t1 = threading.Thread(target=atender_cleinte, args=(client,))
+        t1.start
+
