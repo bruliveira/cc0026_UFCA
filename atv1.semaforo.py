9b50e6dc5669f6afeae8a29fdd0e5193c06ae775
Bruna Oliveira <69090175+bruliveira@users.noreply.github.com>
2022-10-24 09:46:15 -0300

    finalizando

--- atv1.semaforo.py
+++ atv1.semaforo.py
@@ -1,32 +1,34 @@
+'''
+Escreva um programa em Python, que simule a fila de atendimento de um banco. O banco possui 3 caixas. O tempo de atendimento de cada cliente deve ser um tempo aleatório entre 3 a 10 segundos. Suponha que a fila tenha um tamanho fixo com 30 clientes em espera. Utilize um semáforo para fazer o gerenciamento dos recursos compartilhados (caixas) entre os clientes (threads). 
+'''
+
 import threading
 import time
 import random
 
 semaforo = threading.Semaphore(3) #Define a quantidade de threading
 
-def atendimento():
+def atendimento(id):
     semaforo.acquire()
-    print(threading.currentThread().getName())
+    print("Cliente {}".format(id) + " em atendimento")
     time.sleep(random.randint(3,10))
+    print("Finalizando...{}".format(id))
+
     semaforo.release()
-
-def lista_():
-    return list(range(1,31))
     
-
 
 if __name__=="__main__":
-    t1 = threading.Thread(target=atendimento)
-    t2 = threading.Thread(target=atendimento)
-    t3 = threading.Thread(target=atendimento)
-    
-    t1.start()
-    t2.start()
-    t3.start()
 
-    t1.join()
-    t2.join()
-    t3.join()
- 
-print(lista_())
+    threadsaqui =  []
+
+    for i in range(1, 31):
+        theread = threading.Thread(target=atendimento, args=(i, ))
+        threadsaqui.append(theread)
+
+
+    for thread in threadsaqui:
+        thread.start()
+
+    for thread in threadsaqui:
+        thread.join()
 
